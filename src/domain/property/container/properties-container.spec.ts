6d301b6a43b22971d4590a0fbbc680e0d2a98b14
Dzmitry_Kazlou <dzmitry_kazlou1@epam.com>
2022-07-26 22:15:06 +0300

    added tests to container

--- src/domain/property/container/properties-container.spec.ts
+++ src/domain/property/container/properties-container.spec.ts
@@ -12,5 +12,23 @@ describe('PropertiesContainer', () => {
         });
 
         expect(container).toBeTruthy();
-    })
+    });
+
+    it('should get property', () => {
+        const container = new PropertiesContainer<TestProperties>({
+            foo: new NumberProperty(),
+        });
+
+        expect(container.getPropertyValue('foo')).toEqual(0);
+    });
+
+    it('should set and get property', () => {
+        const container = new PropertiesContainer<TestProperties>({
+            foo: new NumberProperty(),
+        });
+
+        container.setPropertyValue('foo', 123);
+
+        expect(container.getPropertyValue('foo')).toEqual(123);
+    });
 });
