6392bd70f27bc89c1f6f98fdd359a71446a69ce1
Dzmitry_Kazlou <dzmitry_kazlou1@epam.com>
2022-08-08 13:00:27 +0300

    fixed lint for PropertiesContainer

--- src/domain/property/container/properties-container.ts
+++ src/domain/property/container/properties-container.ts
@@ -7,33 +7,64 @@ type WritableKeys<T> = {
     [P in keyof T]-?: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, P>
 }[keyof T];
 
-type Get<Properties extends Record<keyof Properties, BaseProperty<unknown>>> = {
+export type PropertiesContainerBase<Properties> = Record<keyof Properties, BaseProperty<unknown>>;
+
+type Get<Properties extends PropertiesContainerBase<Properties>> = {
     [Key in keyof Properties]: () => PropertyValueType<Properties[Key]>;
 };
 
-type Set<Properties extends Record<keyof Properties, BaseProperty<unknown>>> = {
+const isGetValid = <Properties extends PropertiesContainerBase<Properties>>(
+    getInstance: Partial<Get<Properties>>,
+    propertyKeys: Array<keyof Properties>,
+): getInstance is Get<Properties> => propertyKeys.every((propertyKey) => propertyKey in getInstance);
+
+type Set<Properties extends PropertiesContainerBase<Properties>> = {
     [Key in keyof Properties]: (value: PropertyValueType<Properties[Key]>) => void;
 };
 
-export type PropertiesContainerBase<Properties> = Record<keyof Properties, BaseProperty<unknown>>;
+type SetWritable<Properties extends PropertiesContainerBase<Properties>> = Set<Pick<Properties, WritableKeys<Properties>>>;
+
+const isSetValid = <Properties extends PropertiesContainerBase<Properties>>(
+    getInstance: Partial<SetWritable<Properties>>,
+    propertyKeys: Array<keyof Properties>,
+): getInstance is SetWritable<Properties> => propertyKeys.every((propertyKey) => propertyKey in getInstance);
 
 export class PropertiesContainer<Properties extends PropertiesContainerBase<Properties>> {
     public get: Get<Properties>;
-    public set: Set<Pick<Properties, WritableKeys<Properties>>>;
+    public set: SetWritable<Properties>;
 
     constructor(private properties: Properties) {
         const propertyKeys = Object.keys(properties) as Array<keyof Properties>;
-        this.get = propertyKeys.reduce<Get<Properties>>((acc, key) => ({
-            ...acc,
-            [key]: () => this.getPropertyValue(key),
-        }), {} as Get<Properties>);
-        this.set = propertyKeys.reduce<Set<Properties>>((acc, key) => ({
-            ...acc,
-            [key]: (value: PropertyValueType<Properties[typeof key]>) => { this.setPropertyValue(key, value); },
-        }), {} as Set<Properties>);
+        this.get = this.propertiesToGetInstance(propertyKeys);
+        this.set = this.propertiesToSetInstance(propertyKeys);
         propertyKeys.forEach((propertyKey) => {
             properties[propertyKey].owner = this;
         });
+    }
+
+    private propertiesToGetInstance(propertyKeys: Array<keyof Properties>): Get<Properties> {
+        const getInstance = propertyKeys.reduce<Partial<Get<Properties>>>((acc, key) => ({
+            ...acc,
+            [key]: () => this.getPropertyValue(key),
+        }), {});
+        if (isGetValid(getInstance, propertyKeys)) {
+            return getInstance;
+        }
+        throw new Error('"get" instance is not complete');
+    }
+
+    private propertiesToSetInstance(propertyKeys: Array<keyof Properties>): SetWritable<Properties> {
+        const setInstance = propertyKeys.reduce<Partial<SetWritable<Properties>>>((acc, key) => ({
+            ...acc,
+            [key]: (value: PropertyValueType<Properties[typeof key]>) => {
+                this.setPropertyValue(key, value);
+            },
+        }), {});
+
+        if (isSetValid(setInstance, propertyKeys)) {
+            return setInstance;
+        }
+        throw new Error('"set" instance is not complete');
     }
 
     private setPropertyValue<Key extends keyof Properties>(property: Key, value: PropertyValueType<Properties[Key]>): void {
