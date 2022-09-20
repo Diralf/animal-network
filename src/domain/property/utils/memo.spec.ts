c9340a703f301b60a39592bd63831440a2040d33
Dzmitry_Kazlou <dzmitry_kazlou1@epam.com>
2022-09-20 22:37:01 +0300

    updated memo function

--- src/domain/property/utils/memo.spec.ts
+++ src/domain/property/utils/memo.spec.ts
@@ -9,51 +9,46 @@ const getMemoFunc = () => {
 };
 
 describe('memo', () => {
-    it('should return real value', () => {
+    it.each([
+        {
+            case: 'return real value',
+            steps: [{ input: [1, 2], output: 3, cached: false }],
+        },
+        {
+            case: 'return cached value',
+            steps: [
+                { input: [1, 2], output: 3, cached: false },
+                { input: [1, 2], output: 3, cached: true },
+            ],
+        },
+        {
+            case: 'clear cache at size exceeding',
+            steps: [
+                // Called only here before clearing of cache
+                { input: [1, 2], output: 3, cached: false },    // [12]
+                { input: [2, 2], output: 4, cached: false },    // [22, 12]
+                { input: [2, 2], output: 4, cached: true },     // [22, 12]
+                { input: [2, 3], output: 5, cached: false },    // [23, 22, 12]
+                // Cache should be cleared after this call
+                { input: [3, 3], output: 6, cached: false },    // [33, 23, 22]
+                { input: [2, 3], output: 5, cached: true },     // [23, 33, 22]
+                // After cache clear those parameter calls real function
+                { input: [1, 2], output: 3, cached: false },    // [12, 33, 23]
+                { input: [2, 2], output: 4, cached: false },
+            ],
+        },
+    ])('should $case', ({ steps }) => {
         const { memoFunc, originalFuncSpy } = getMemoFunc();
 
-        expect(memoFunc.call(1, 2)).toEqual(3);
-        expect(originalFuncSpy).toHaveBeenCalledWith(1, 2);
-    });
-
-    it('should return cached value', () => {
-        const { memoFunc, originalFuncSpy } = getMemoFunc();
-
-        expect(memoFunc.call(1, 2)).toEqual(3);
-        expect(originalFuncSpy).toHaveBeenCalledTimes(1);
-        expect(originalFuncSpy).toHaveBeenCalledWith(1, 2);
-
-        expect(memoFunc.call(1, 2)).toEqual(3);
-        expect(originalFuncSpy).toHaveBeenCalledTimes(1);
-    });
-
-    it('should clear cache', () => {
-        const { memoFunc, originalFuncSpy } = getMemoFunc();
-
-        // Called only here before clearing of cache
-        expect(memoFunc.call(1, 2)).toEqual(3);
-        expect(originalFuncSpy).toHaveBeenCalledTimes(1);
-        expect(originalFuncSpy).toHaveBeenCalledWith(1, 2);
-
-        expect(memoFunc.call(2, 2)).toEqual(4);
-        expect(originalFuncSpy).toHaveBeenCalledTimes(2);
-        expect(originalFuncSpy).toHaveBeenCalledWith(2, 2);
-
-        expect(memoFunc.call(2, 2)).toEqual(4);
-        expect(originalFuncSpy).toHaveBeenCalledTimes(2);
-
-        expect(memoFunc.call(2, 3)).toEqual(5);
-        expect(originalFuncSpy).toHaveBeenCalledTimes(3);
-        expect(originalFuncSpy).toHaveBeenCalledWith(2, 3);
-
-        // Cache should be cleared after this call
-        expect(memoFunc.call(3, 3)).toEqual(6);
-        expect(originalFuncSpy).toHaveBeenCalledTimes(4);
-        expect(originalFuncSpy).toHaveBeenCalledWith(3, 3);
-
-        // After cache clear those parameter calls real function
-        expect(memoFunc.call(1, 2)).toEqual(3);
-        expect(originalFuncSpy).toHaveBeenCalledTimes(5);
-        expect(originalFuncSpy).toHaveBeenCalledWith(1, 2);
+        steps.forEach(({ input, output, cached }) => {
+            expect(memoFunc.call(...input)).toEqual(output);
+            if (cached) {
+                expect(originalFuncSpy).not.toHaveBeenCalled();
+            } else {
+                expect(originalFuncSpy).toHaveBeenCalledWith(...input);
+                expect(originalFuncSpy).toHaveBeenCalledTimes(1);
+            }
+            originalFuncSpy.mockClear();
+        });
     });
 });
--- src/domain/property/utils/memo.ts
+++ src/domain/property/utils/memo.ts
@@ -1,26 +1,34 @@
 export const memo = <Input extends unknown[], Output>(func: (...args: Input) => Output, cacheMaxSize = 1000) => {
     const cache = new Map<string, Output>();
-    const wasUsed = new Set<string>();
+    const usage: string[] = [];
+
+    const addToCache = (key: string, result: Output) => {
+        cache.set(key, result);
+
+        const previousUsageIndex = usage.indexOf(key);
+        if (previousUsageIndex >= 0) {
+            usage.splice(previousUsageIndex, 1);
+        }
+        usage.unshift(key);
+
+        if (usage.length > cacheMaxSize) {
+            const keysToDelete = usage.splice(cacheMaxSize, usage.length - cacheMaxSize);
+            keysToDelete.forEach((key) => {
+                cache.delete(key);
+            });
+        }
+    };
 
     return {
         call: (...args: Input) => {
             const key = JSON.stringify(args);
             const cacheValue = cache.get(key);
             if (cacheValue) {
-                wasUsed.add(key);
                 return cacheValue;
             }
             const result = func(...args);
-            cache.set(key, result);
 
-            if (cache.size > cacheMaxSize) {
-                const keys = Array.from(cache).map(([key]) => key);
-                keys.forEach((key) => {
-                    if (!wasUsed.has(key)) {
-                        cache.delete(key);
-                    }
-                });
-            }
+            addToCache(key, result);
 
             return result;
         },
