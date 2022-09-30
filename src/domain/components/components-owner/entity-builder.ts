1166c8ea449d791e5f47ac1231f1c2b6444fcc9b
Dzmitry_Kazlou <dzmitry_kazlou1@epam.com>
2022-09-30 18:10:09 +0300

    improved entity builder

--- src/domain/components/components-owner/entity-builder.ts
+++ src/domain/components/components-owner/entity-builder.ts
@@ -1,9 +1,14 @@
 import { NumberProperty } from '../../property/number/number-property';
 import { UnknownComponent, ComponentPropsType } from './components-owner';
 
-type Builder<Components extends Record<keyof Components, UnknownComponent>> = {
-    [Key in keyof Components]: (props: ComponentPropsType<Components[Key]>) => Builder<Components>
+interface Build<Remain, Original> {
+    build: () => Omit<Original, keyof Remain>;
+}
+
+type Builder<Components extends Record<keyof Components, UnknownComponent>, Original = Components> = {
+    [Key in keyof Components]: (props: ComponentPropsType<Components[Key]>) => Builder<Omit<Components, Key>, Original> & Build<Omit<Components, Key>, Original>;
 };
+
 
 export function builder<Components extends Record<keyof Components, UnknownComponent>>(): Builder<Components> {
     // @ts-ignore
@@ -13,8 +18,18 @@ export function builder<Components extends Record<keyof Components, UnknownCompo
 interface Foo {
     field1: NumberProperty;
     field2: NumberProperty;
+    field3: NumberProperty;
+    field4: NumberProperty;
 }
 
-builder<Foo>()
+const aaa = builder<Foo>()
     .field1({ current: 1 })
     .field2({ current: 2 })
+    .build();
+
+aaa.field1;
+aaa.field2;
+// @ts-expect-error
+aaa.field3;
+// @ts-expect-error
+aaa.field4;
